1b1cc40d744bf963f8ebb34e29e6c74dcb26a0c2
Shaksham Karki <shakshamkarki@Shakshams-MacBook-Pro.local>
2025-08-02 10:25:07 +0545

    Completed Tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,41 @@
-import { Tabs } from "expo-router";
+import { Tabs } from 'expo-router';
 
-export default function TabsasLayout() {
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+
+export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{
-        headerTitle: "StickerSmash", 
-        headerLeft:()=> <> </>
-      }}/>
-      <Tabs.Screen name="about" options={{
-        headerTitle:" About"
-      }}/>
-    <Tabs.Screen
-    name="not-found"
-    options={{
-      headerShown:false,
-    }}/>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#ffd33d',
+        headerStyle:{
+            backgroundColor: "#25292e",
+        },
+        headerShadowVisible:false,
+        headerTintColor: "#fff",
+        tabBarStyle: {
+            backgroundColor : "#25292e",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
+          ),
+        }}
+      />
     </Tabs>
-  )
+  );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
-import { LogBox } from "react-native";
+import { LogBox, } from "react-native";
+import {StatusBar} from "expo-status-bar" ;
 
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
+    <>
+    <StatusBar style="light" />
+    
     <Stack>
       <Stack.Screen name="(tabs)" options={{
         headerShown :false,
@@ -15,5 +19,6 @@ export default function RootLayout() {
       headerShown:false,
     }}/>
     </Stack>
+    </>
   )
 }
