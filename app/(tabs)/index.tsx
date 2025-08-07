145338ec734a8c30029630dfc56eac6888e3f09d
Shaksham Karki <shakshamkarki@Shakshams-MacBook-Pro.local>
2025-08-07 14:07:01 +0545

    made Emoji components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,24 @@
+import { ImageSourcePropType, View, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { StyleSheet, View } from 'react-native';
 import { useState } from 'react';
+
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
 import IconButton from '@/components/IconButton';
 import CircleButton from '@/components/CircleButton';
+import EmojiPicker from '@/components/EmojiPicker';
+import EmjoiSticker from '@/components/EmojiSticker';
+
+import EmojiList from '@/components/EmojiList';
+
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
-
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
-
-  const [showAppOptions, setShowAppOptions]=useState<boolean>(false);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | undefined>(undefined);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -23,49 +29,51 @@ export default function Index() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any image.');
     }
   };
 
-  const onReset=()=>{
+  const onReset = () => {
     setShowAppOptions(false);
   };
 
-  const onAddSticker=()=>{
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
 
-  }
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
 
-  const onSaveImageAsync=async()=>{
-
+  const onSaveImageAsync = async () => {
+    // we will implement this later
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={selectedImage ||PlaceholderImage}  />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+        {pickedEmoji && <EmjoiSticker imageSize={40} stickerSource={pickedEmoji} /> }
       </View>
-      {showAppOptions? (
-
+      {showAppOptions ? (
         <View style={styles.optionsContainer}>
           <View style={styles.optionsRow}>
-          <IconButton icon='refresh' label ='Reset' onPress ={onReset}/>
-          <CircleButton onPress={onAddSticker} />
-          <IconButton icon='savee-alt' label='Save' onPress={onSaveImageAsync} />
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
           </View>
-          </View>
-      ):(
-      <View style={styles.footerContainer}>
-        <Button theme="primary"
-         label="Choose a photo" 
-         onPress={pickImageAsync}
-          />
-        <Button 
-        label="Use this photo" 
-        onPress={()=> setShowAppOptions(true)}
-        />
-      </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
       )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
     </View>
   );
 }
@@ -83,13 +91,12 @@ const styles = StyleSheet.create({
     flex: 1 / 3,
     alignItems: 'center',
   },
-  optionsContainer:{
-   position:'absolute',
-   bottom: 80,
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
   },
-  optionsRow:{
-    alignItems:"center",
-    flexDirection:'row',
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
-
